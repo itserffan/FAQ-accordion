c163e167965c8265b4b9adc044c16b24bfa927f0
Erfan <96052531+erfansoleimanii@users.noreply.github.com>
2024-05-03 23:30:26 +0330

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const btn = document.querySelectorAll(".btn");
 const icon = document.querySelectorAll(".icon");
 const answer = document.querySelectorAll(".hide-answer");
+const question = document.querySelectorAll(".question");
 
 for (let i = 0; i < btn.length; i++) {
   btn[i].addEventListener("click", function () {
@@ -11,5 +12,15 @@ for (let i = 0; i < btn.length; i++) {
     icon[i].classList.toggle("icon-close");
     icon[i].classList.toggle("icon-open");
     answer[i].classList.toggle("hide-answer");
+    question[i].classList.toggle("question-selected");
+  });
+
+  question[i].addEventListener("click", function () {
+    btn[i].classList.toggle("btn-open");
+    btn[i].classList.toggle("btn-close");
+    icon[i].classList.toggle("icon-close");
+    icon[i].classList.toggle("icon-open");
+    answer[i].classList.toggle("hide-answer");
+    question[i].classList.toggle("question-selected");
   });
 }
